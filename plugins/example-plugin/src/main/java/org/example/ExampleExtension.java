d1e34aa83b8a0c7f2acb31656bc7c4812bcbd3d1
Anders Råberg <anders.raberg@gmail.com>
2025-07-30 16:24:38 +0200

    Remove unused imports

--- plugins/example-plugin/src/main/java/org/example/ExampleExtension.java
+++ plugins/example-plugin/src/main/java/org/example/ExampleExtension.java
@@ -3,11 +3,9 @@ package org.example;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.OutputDirectory;
 
 public interface ExampleExtension {
-	Property<FileCollection> getFiles();
+    Property<FileCollection> getFiles();
 
-	DirectoryProperty getOutputDir();
+    DirectoryProperty getOutputDir();
 }
